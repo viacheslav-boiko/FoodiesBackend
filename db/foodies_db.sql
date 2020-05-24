792f7d4ce8c2dc426c40d63e54f7e23714d06f3f
seregacarbon <ekvaserega@gmail.com>
2020-05-25 01:26:38 +0300

    updated-mysql-schema

--- db/foodies_db.sql
+++ db/foodies_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 192.168.1.3:3306
--- Время создания: Апр 28 2020 г., 23:04
+-- Время создания: Май 25 2020 г., 01:25
 -- Версия сервера: 8.0.19
 -- Версия PHP: 7.3.16
 
@@ -79,13 +79,6 @@ CREATE TABLE `comment` (
   `pubtime` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Дамп данных таблицы `comment`
---
-
-INSERT INTO `comment` (`id`, `post_id`, `user_id`, `text`, `pubtime`) VALUES
-(1, 4, 1, 'It\'s comment', '2020-04-26 21:56:06');
-
 -- --------------------------------------------------------
 
 --
@@ -119,17 +112,8 @@ CREATE TABLE `detail_image` (
   `id` bigint UNSIGNED NOT NULL,
   `post_id` bigint UNSIGNED NOT NULL,
   `num` tinyint UNSIGNED NOT NULL,
-  `image` varchar(200) COLLATE utf8mb4_general_ci NOT NULL,
   `description` varchar(2000) COLLATE utf8mb4_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Дамп данных таблицы `detail_image`
---
-
-INSERT INTO `detail_image` (`id`, `post_id`, `num`, `image`, `description`) VALUES
-(1, 1, 1, '/users/user_1/post_1/images/1.png', 'fvfdfd'),
-(2, 1, 2, '/users/user_1/post_1/images/2.png', 'fvdfdvv');
 
 -- --------------------------------------------------------
 
@@ -166,15 +150,6 @@ CREATE TABLE `liked` (
   `user_id` bigint UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Дамп данных таблицы `liked`
---
-
-INSERT INTO `liked` (`id`, `post_id`, `user_id`) VALUES
-(1, 2, 32),
-(2, 2, 38),
-(3, 6, 1);
-
 -- --------------------------------------------------------
 
 --
@@ -185,7 +160,6 @@ CREATE TABLE `post` (
   `id` bigint UNSIGNED NOT NULL,
   `title` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `cooktime` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `image` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `description` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `pubtime` datetime NOT NULL,
   `shares` int UNSIGNED NOT NULL,
@@ -199,18 +173,6 @@ CREATE TABLE `post` (
   `category_id` int UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Дамп данных таблицы `post`
---
-
-INSERT INTO `post` (`id`, `title`, `cooktime`, `image`, `description`, `pubtime`, `shares`, `likes`, `user_id`, `proteins`, `lipids`, `carbohydrates`, `calorific`, `cuisine_id`, `category_id`) VALUES
-(1, 'My post 1', '30хв', 'none', 'dfdfgfdgdfbfb\r\ndfbbdfv\r\nbfbdb', '2020-04-20 00:00:00', 12, 13, 1, 23, 44, 22, 124, 1, 2),
-(2, 'My post 2', '1 год', 'None', 'vvfdvf', '2020-04-19 00:04:00', 12, 43, 2, 23, 44, 22, 124, 2, 1),
-(3, 'Post 3', '33', 'None', 'ddsvsdsvdsvdvs', '2020-04-19 00:00:00', 12, 23, 2, 12, 32, 12, 32, 1, 3),
-(4, 'Post 4', '30 хв', 'aaaaaaaaaaaaaaa', 'fdbbnytnnytnytnty', '2020-04-20 13:50:57', 12, 432, 1, 23, 44, 22, 124, 3, 2),
-(5, 'Best post 5', '1 год', 'None', 'ffdbukkyuikuyuku', '2020-04-19 14:00:30', 456, 123, 3, 23, 43, 23, 45644, 2, 1),
-(6, 'Users post 6', '33', 'None', 'dfgfdfdbfdb', '2020-04-20 14:05:30', 34, 2, 3, 124, 432, 34, 4532, 3, 3);
-
 -- --------------------------------------------------------
 
 --
@@ -222,20 +184,6 @@ CREATE TABLE `post_assignment` (
   `post_id` bigint UNSIGNED NOT NULL,
   `assignment_id` int UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Дамп данных таблицы `post_assignment`
---
-
-INSERT INTO `post_assignment` (`id`, `post_id`, `assignment_id`) VALUES
-(1, 1, 3),
-(2, 1, 3),
-(3, 5, 5),
-(4, 1, 2),
-(5, 4, 2),
-(6, 4, 3),
-(7, 6, 2),
-(8, 3, 2);
 
 -- --------------------------------------------------------
 
@@ -251,20 +199,6 @@ CREATE TABLE `post_ingredient` (
   `unit` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Дамп данных таблицы `post_ingredient`
---
-
-INSERT INTO `post_ingredient` (`id`, `post_id`, `ingredient_id`, `amount`, `unit`) VALUES
-(1, 1, 2, 100, 'g'),
-(2, 1, 3, 3, 'i'),
-(3, 2, 4, 123, 'mg'),
-(4, 2, 6, 2, 'st'),
-(5, 3, 4, 140, 'g'),
-(6, 3, 1, 3, 'sp'),
-(7, 4, 4, 100, 'g'),
-(8, 6, 2, 3, 'i');
-
 -- --------------------------------------------------------
 
 --
@@ -273,25 +207,20 @@ INSERT INTO `post_ingredient` (`id`, `post_id`, `ingredient_id`, `amount`, `unit
 
 CREATE TABLE `user` (
   `id` bigint UNSIGNED NOT NULL,
-  `name` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `picture` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `locale` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `email` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `last_signed_time` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Дамп данных таблицы `user`
 --
 
-INSERT INTO `user` (`id`, `name`, `picture`, `locale`, `email`) VALUES
-(1, 'Boiko', 'Not selected', 'uk', 'ekva@gmail.com'),
-(2, 'serega', 'bdfdbfdb', 'Russia', 'fdfdb'),
-(3, 'user', 'None', 'pl', 'user@gmail.com'),
-(7, 'SERGEO', 'None', 'Ukraine', 'email@com'),
-(32, 'NEWUSER1', 'dfgfdg', 'Ukraine', 'dublemail@com'),
-(38, 'New user 1', 'None', 'uk', 'newusder@gmail.com'),
-(132, 'В\'ячеслав Бойко', 'https://lh4.googleusercontent.com/-C8idGcU_x9g/AAAAAAAAAAI/AAAAAAAAAAA/AAKWJJNzMIa4YnxJaCtIc5jkPH_rnCyogQ/s96-c/photo.jpg', 'uk', 'bviacheslav.12@gmail.com'),
-(141, 'Serega Carbon', 'https://lh3.googleusercontent.com/a-/AOh14Gj3t4Q0FN6vxGlRutvAaLRIa3QC7BdmJO84_g1zDg=s96-c', 'uk', 'ekvaserega@gmail.com');
+INSERT INTO `user` (`id`, `name`, `picture`, `locale`, `email`, `last_signed_time`) VALUES
+(4, 'Serega Carbon', 'https://lh3.googleusercontent.com/a-/AOh14Gj3t4Q0FN6vxGlRutvAaLRIa3QC7BdmJO84_g1zDg=s96-c', 'uk', 'ekvaserega@gmail.com', '2020-05-25 01:23:32'),
+(5, 'В\'ячеслав Бойко', 'https://lh4.googleusercontent.com/-C8idGcU_x9g/AAAAAAAAAAI/AAAAAAAAAAA/AMZuucksCikOLZo5VkftcKdYJbCWXmc2Jw/s96-c/photo.jpg', 'uk', 'bviacheslav.12@gmail.com', '2020-05-25 01:18:49');
 
 --
 -- Индексы сохранённых таблиц
@@ -426,25 +355,25 @@ ALTER TABLE `liked`
 -- AUTO_INCREMENT для таблицы `post`
 --
 ALTER TABLE `post`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT для таблицы `post_assignment`
 --
 ALTER TABLE `post_assignment`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT для таблицы `post_ingredient`
 --
 ALTER TABLE `post_ingredient`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT для таблицы `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=142;
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Ограничения внешнего ключа сохраненных таблиц
